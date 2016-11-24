614d72bcaf149fdf494ffb63e82c7f2c25ba9c67
Craig Webster <craig.webster@mac.com>
2016-11-24 22:10:10 +0000

    Initial Commit

---
+++ ReferralLaboratories/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ReferralLaboratories
+//
+//  Created by Craig Webster on 24/11/2016.
+//  Copyright © 2016 Craig Webster. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
